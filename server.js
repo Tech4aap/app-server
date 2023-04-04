cf33f4271ffa0fc94c95d2f89d6818c7b933d96e
Muhammad Fahad <121437478+fahad-jr-fsd@users.noreply.github.com>
2023-04-04 12:23:40 +0500

    new chnages

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const mongoose = require("mongoose");
 const cors = require("cors");
+const PDFDocument = require('pdfkit-table');
 
 const app = express();
 
@@ -39,15 +40,59 @@ app.get('/version_:v', async (req, res) =>{
   }
 });
 
+app.get('/new', async (req, res) =>{
+  try {
 
+    const doc = new PDFDocument({});
+
+    // generateHeader(doc);
+    // // const table = 
+    
+    const table = {
+      title: "1. BASIC INFORMATION",
+      
+      headers: [
+        { label: "Id", property: 'number', width: 20, renderer: null },
+        { label: "Name", property: 'name', width: 280, renderer: null },
+        { label: "Description", property: 'description', width: 280, renderer: null },      
+      ],
+      // complex data
+      datas: [
+        { 
+          number: 1,
+          name: 'Employee Name', 
+          description: 'Hassan', 
+        },
+      ],
+      // simeple data
+      
+    };
+    // the magic
+    doc.table(table, {
+      prepareHeader: () => doc.font("Helvetica-Bold").fontSize(8),
+  
+    });
+  
+    doc.pipe(res); 
+   
+    doc.end();
+   
+    res.writeHead(200, {
+      'Content-Type': 'application/pdf',
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 app.use("/api/yarns", require("./routes/yarn"));
 app.use("/api/product", require("./routes/product"));
 app.use("/api/report", require("./routes/reportConstant"));
 app.use("/api/details/", require("./routes/reportDetails"));
-app.use("/api/new/", require("./routes/report"));
-app.use("/api/pdf/", require("./routes/pdf"));
-app.use("/api/", require("./routes/generatePdf"));
+// app.use("/api/new/", require("./routes/report"));
+// app.use("/api/pdf/", require("./routes/pdf"));
+
 
 
 
