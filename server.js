2dadf5498ee3ba4ba5df11215b9d53c048c56126
HassanNoman6969 <132579793+HassanNoman6969@users.noreply.github.com>
2024-07-18 14:18:57 +0500

    changes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) =>{
   try {
-    res.status(200).json("v1");
+    res.status(200).json({ "version": 0.1 });
   } catch (err) {
     res.status(500).json(err);
   }
